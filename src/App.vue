d24c1110bbdc0c8152b8ef6416355213c089e0ee
Kandianne <kandipierre@gmail.com>
2019-09-18 21:23:19 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <a href="https://www.movinghelp.com/movers/raleigh-nc-27606/90476?agnosticDetails=False&orderType=1&jobActivityType=0">
+    <img alt="Careful Contents Movers" src="./assets/logo.png">
+    </a>
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+  height: 900px;
+  overflow: hidden;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: #d9dada;
+  margin: 0px;
+  height: 100%;
+  padding-top: 30%;
+}
+img {
+  width:150px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+ <h2>
+</h2>
+<div>
+  <a href="https://www.movinghelp.com/movers/raleigh-nc-27606/90476?agnosticDetails=False&orderType=1&jobActivityType=0">
+    Thank you for visiting! Our website is currently under construction, but click here to check us out!
+  </a> 
+</div>
+<div>
+    <a href="mailto:name@mydomain.com">carefulcontentsmovers@gmail.com</a>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+div {
+  padding: 30px 0px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: black;
+  text-decoration: none;
+}
+</style>
