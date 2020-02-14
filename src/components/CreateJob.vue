15b408f535ee4c23387c4847e9f179362e1860f6
Kandianne Pierre <Kandianne.Pierre@ibm.com@Kandiannes-MBP.attlocal.net>
2020-02-13 23:10:01 -0500

    wip form

--- src/components/CreateJob.vue
+++ src/components/CreateJob.vue
@@ -2,12 +2,13 @@
   <v-form>
     <v-container>
       <v-row>
-        <v-col v-for="q in questions" v-bind:key="q.name"
+        <!-- <v-col v-for="q in questions" v-bind:key="q.name" -->
+        <v-col
           cols="12"
           md="4"
         >
           <v-text-field
-            v-model="q.text"
+            v-model="firstName"
             :counter="10"
             label="First name"
             required
@@ -19,8 +20,7 @@
           md="4"
         >
           <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
+            v-model="lastName"
             :counter="10"
             label="Last name"
             required
@@ -33,23 +33,71 @@
         >
           <v-text-field
             v-model="phone"
-            :rules="phoneRules"
             label="Phone"
             required
           ></v-text-field>
         </v-col>
       </v-row>
       <v-row>
-          <v-radio-group v-model="moveOptions">
-        <v-col>
+          <v-radio-group v-model="moveOptions" row>
             <v-radio
               v-for="n in moveOptions"
               :key="n"
               :label="n"
               :value="n"
             ></v-radio>
-        </v-col>
           </v-radio-group>
+      </v-row>
+      <v-row>
+      <v-col cols="12" md="12">
+          <v-textarea
+            outlined
+            name="input-7-4"
+            label="Brief description"
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-text-field
+            v-model="address"
+            :counter="10"
+            label="Street address"
+            required
+          ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-text-field
+            v-model="address2"
+            :counter="10"
+            label="Street address 2"
+            required
+          ></v-text-field>
+      </v-row>
+      <v-row>
+      <v-col cols="12" md="4">
+        <v-text-field
+            v-model="city"
+            :counter="10"
+            label="City"
+            required
+          ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="4">
+          <v-text-field
+            v-model="state"
+            :counter="10"
+            label="State"
+            required
+          ></v-text-field>
+      </v-col>
+      <v-col cols="12" md="4">
+           <v-text-field
+            v-model="zipCOde"
+            :counter="10"
+            label="Zip Code"
+            required
+          ></v-text-field>
+      </v-col>
       </v-row>
     </v-container>
   </v-form>
@@ -60,21 +108,21 @@
     r: () => ({
       requiredRule: [
         v => !!v || 'Required',
-      ],
-      radioGroup: 1,
+      ]      
+    }),
+    data: () => ({
+      // questions: [
+      //   {name: "date", text: "When do you want to move?" },
+      //   {name: "hours-estimated", text: "How many hours of help do you need?"},
+      //   {name: "address", text: "Where can we meet you?"}
+      // ],
       moveOptions: [
         "Load",
         "Unload",
         "Both",
         "Other"
-      ]
-    }),
-    data: () => ({
-      questions: [
-        {name: "date", text: "When do you want to move?" },
-        {name: "hours-estimated", text: "How many hours of help do you need?"},
-        {name: "address", text: "Where can we meet you?"}
-      ]
+      ],
+      radioGroup: 1
     }),
   }
 </script>
